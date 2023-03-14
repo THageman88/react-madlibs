d6e67584221fec0a6d47c42493200ac904947b30
Timmy Tompkins <timhageman@Clementine.lan>
2023-03-14 15:47:52 -0500

    concept and decimal placement

--- react-1/addCommas.js
+++ react-1/addCommas.js
@@ -1,5 +1,12 @@
 function addCommas(num) {
-     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  // Convert the input number to a string using the toString() method.
+  let str = num.toString();
+  // Define a regular expression that matches non-word boundaries (\B) followed by groups of three digits.
+  // The (?=(\d{3})+(?!\d)) lookahead assertion ensures that the match is followed by another group of three digits, but not another digit.
+  let pattern = /\B(?=(\d{3})+(?!\d))/g;
+  // Use the replace() method to insert commas between groups of three digits.
+  // The pattern is replaced with a comma followed by the match, represented by $&.
+  return str.replace(pattern, ",");
 }
 
 module.exports = addCommas;
--- react-1/addCommas.test.js
+++ react-1/addCommas.test.js
@@ -1,7 +1,11 @@
 const addCommas = require("./addCommas");
 
 describe("#addCommas", () => {
-  test("it is a function", () => {
-    expect(typeof addCommas).toBe("function");
+  test("it should add commas to numbers", () => {
+    expect(addCommas(1000)).toBe("1,000");
+    expect(addCommas(-1000)).toBe("-1,000");
+    expect(addCommas(14000000)).toBe("14,000,000")
   });
 });
+
+
